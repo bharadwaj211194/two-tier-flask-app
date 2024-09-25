36f065204d3bd01ec2a87fa3fe87985be96131ff
bharadwaj211194 <vsnikhilbharadwajv@gmail.com>
2024-09-25 14:58:20 +0530

    Update backend.tf

--- Jenksin-terraform/backend.tf
+++ Jenksin-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "ajay-mrcloudbook777" # Replace with your actual S3 bucket name
-    key    = "Jenkins/terraform.tfstate"
-    region = "ap-south-1"
+    bucket = "demo-bucket-25-09-2024" # Replace with your actual S3 bucket name
+    key    = "Jenkins-Nikhil/terraform.tfstate"
+    region = "us-east-1"
   }
 }
